920624b29409079346939829975ab10e7c984346
maayan moses <maayanmoses@maayans-MacBook-Pro.local>
2025-06-10 16:36:48 +0300

    z-index of modal

--- src/components/ModalWrapper.vue
+++ src/components/ModalWrapper.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="fixed inset-0 bg-black/70 z-[50] w-full h-[100vh]  overflow-y-auto flex items-center">
+  <div class="fixed inset-0 bg-black/70 z-[150] w-full h-[100vh]  overflow-y-auto flex items-center">
     <div class="relative flex flex-col my-auto  md:w-[60%] w-[90%] mx-auto rounded-xl  shadow-md overflow-hidden border-2 bg-gray-100">
       <button
         class="mt-2 ml-2 self-end font-semibold text-gray-600 hover:bg-gray-300 p-2 rounded-lg text-xl"
